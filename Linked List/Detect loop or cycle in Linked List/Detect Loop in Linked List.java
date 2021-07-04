df99571c144ccb98550fb9a5bc3571602070059f
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-04 10:59:56 +0530

    Detect Loop in Linked List.java

---
+++ Linked List/Detect loop or cycle in Linked List/Detect Loop in Linked List.java
@@ -0,0 +1,109 @@
+// { Driver Code Starts
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // your code here
+        // using floyds Algorithm
+        if(head == null){
+            return false;
+        }
+        
+        // creating 2 linked list
+        // one is of slow which points to next node
+        // other is of fast which points to 2 next node
+        Node slow = head;
+        Node fast = head.next;
+        
+        while(slow != fast){
+            
+            // if fast is null or fast.next is null then it doesn't have a loop 
+            if( fast == null || fast.next == null){
+                return false;
+            }
+            
+            // points to next node
+            slow = slow.next;
+                
+            // points to 2nd next node
+            fast = fast.next.next;
+            
+                
+        }
+        return true;
+    }
+}
