eca32b0a764ed6b76f9875a212cb6c7223262256
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-10 16:27:37 +0530

    Leetcode & interviewbit

---
+++ Integer/FizzBuzz/Fizz Buzz by String Concat.cpp
@@ -0,0 +1,36 @@
+// Solution 2 in C++ - (Faster one)
+// Method = String Concatenation
+// Time Complexity  T = o(n), worst case
+// Space Complexity  S = o(1) 
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+          // creating a String array list
+        vector<string> ans;
+        
+        // creating a loop which run for n time, since the loop starts with 1
+        for(int i = 1; i<=n; i++){
+            string ansStr = "";
+            
+            // checking if i is divisible by 3 or not, if divisible add "FizzBuzz" to the array list
+            if( i % 3 == 0){
+                ansStr += "Fizz";
+            }
+            
+            // checking if i is divisible by 5 or not, if divisible add "Buzz" to the array list
+            if(i % 5 == 0){
+                ansStr += "Buzz";
+            }
+            
+            // if i is not divisibe by 3 or 5, 3 and 5 then add up the i value in the form of string to the arrayList
+            if( ansStr == ""){
+                ansStr += to_string(i);
+            }
+            
+            ans.push_back(ansStr);
+        }
+        return ans;
+    }
+};
+
