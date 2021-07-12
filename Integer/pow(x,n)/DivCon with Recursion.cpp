d9a54b21790bf141af7ccb9acdff67671269474b
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-12 17:50:43 +0530

    GeeksForGeeks

---
+++ Integer/pow(x,n)/DivCon with Recursion.cpp
@@ -0,0 +1,43 @@
+// Method = Divide and conquer with recursion
+// Time Complexity T = O(log (n))
+// Space Complexity S = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+float power(float x, int y)
+{
+	
+  float temp;
+	
+  if(y == 0)
+		return 1;
+	
+  temp = power(x, y / 2);
+	
+  if (y % 2 == 0)
+		return temp * temp;
+	
+  else
+	{
+	
+    if(y > 0)
+		
+      return x * temp * temp;
+		
+    else
+		 	
+      return (temp * temp) / x;
+	}
+
+}
+
+// Driver Code
+int main()
+{
+	float x = 2;
+	int y = -3;
+	cout << power(x, y);
+	return 0;
+}
+
