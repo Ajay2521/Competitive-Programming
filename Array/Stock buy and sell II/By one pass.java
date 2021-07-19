ee6a5123559737a56794900882e9acea7b4f6925
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-19 13:34:59 +0530

    Leetcode, GeeksForGeeks, InterviewBit

---
+++ Array/Stock buy and sell II/By one pass.java
@@ -0,0 +1,26 @@
+// Method - One Pass(Optimised)
+// Time Complexity T - O(n)
+// Space Complexity S - O(1)
+
+
+class Solution {
+    public int maxProfit(int[] prices) {        
+        
+        // initialise the maximumProfit to "0"
+        int maximumProfit = 0;
+        
+        // loop throug the price array from "1" to "n"
+        for(int i = 1; i < prices.length ; i++){
+            
+            // ensure that the "i-th" element is always greater than the previious "i-1-th" element
+            if (prices[i] > prices[i - 1]){
+                
+                // adding up the maximum value and difference between current and previous element
+                maximumProfit = maximumProfit + prices[i] - prices[i-1];
+            }
+        }    
+        // return the maximum transaction
+        return maximumProfit;
+    }
+}
+
