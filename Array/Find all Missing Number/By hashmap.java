c2187eb0c8ce0938904b92f753e732e2d1ec1fd6
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-20 12:08:08 +0530

    Leetcode, GeeksForGeeks

---
+++ Array/Find all Missing Number/By hashmap.java
@@ -0,0 +1,36 @@
+// Method - Hashing(BruteForce)
+// Time Complexity T - O(n)
+// Space Complexity  S - O(n) 
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        // initializing the missingNum variable
+        List<Integer> missingNum = new ArrayList<>();
+        
+        // initializing the number varibale which makes copy of nums
+        Set<Integer> numbers = new HashSet<>();
+        
+        // looping through the array "0" to "n-1"
+        for(int i = 0; i < nums.length; i++){
+            // adding the unique number to the set
+            numbers.add(nums[i]);
+        }
+        
+        // looping through the array "1" to "n"
+        for(int i = 1; i <= nums.length; i++){
+        
+            // check the presence of "i" in the set
+            // if "i" is present in the set, ignore
+            // if "i" is not present in the set, 
+            // then insert the "i" in the missingNum
+            if(!numbers.contains(i)){
+                missingNum.add(i);    
+            }
+        }
+        
+        // return the mising numbers        
+        return missingNum;
+    }
+}
