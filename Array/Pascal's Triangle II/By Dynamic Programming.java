8044ca9eae3074efd175c3fdc21003dde55d9ded
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-25 12:41:37 +0530

    Leetcode

---
+++ Array/Pascal's Triangle II/By Dynamic Programming.java
@@ -0,0 +1,24 @@
+// Method = Dynamic Programming
+// Time Complexity T = O(n^2)
+// Space Complexity T = O(k)
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+    
+        // initialize the result
+        List<Integer> result = new ArrayList<Integer>();
+        
+        for(int i = 0; i <= rowIndex; i++) {
+            
+            // adding the 1 to the first index 
+            result.add(1);
+            
+            for(int j = i - 1; j > 0; j--) {
+                result.set(j, result.get(j-1) + result.get(j));
+            }
+        }
+        
+        // return the result
+        return result;
+    }
+}
