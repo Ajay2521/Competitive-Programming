cf5393745531bdfaf4adc2d5cd047013b868dc07
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-25 09:36:05 +0530

    Leetcode

---
+++ Array/Merge Sorted Array/By Iterating & Insertion.java
@@ -0,0 +1,37 @@
+// Method - Iterating from last element(Optimised)
+// Time complexity T = O(n)
+// Space Complexity S = O(1)
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        // initialize the value of "m" and "n" to access the last element of an "nums1" and "nums2" array
+        m = m-1;
+        n = n-1;
+        
+        // looping from "m+n+1" to 0
+        for(int i = m+n-1; i>=0 ; i-- ){
+            
+            // getting the last element of an array "nums1" and "nums2"
+            // assigning the minium integer value, to avoid run time error
+            int a = m >= 0 ? nums1[m] : Integer.MIN_VALUE;
+            int b = n >= 0 ? nums2[n] : Integer.MIN_VALUE;
+          
+            
+            // if the element of nums1 > num2
+            // then place the nums1 element at the last place(num1[i]), and decrement the value of "m"
+            if (a>b){
+                nums1[i] = a;
+                m--;
+            }
+            
+            // if the element of nums1 < num2
+            // then place the nums2 element at the last place(num1[i]), and decrement the value of "n"
+            else{
+                nums1[i] = b;
+                n--;
+            }
+        }
+    }
+}
