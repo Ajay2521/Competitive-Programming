889d1200517630c615dede3599842e65291e1436
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-28 07:52:07 +0530

    Leetcode, GeeksForGeeks, InterviewBit

---
+++ Array/Group Anagrams/By hashmap.java
@@ -0,0 +1,44 @@
+// Method - Hashmap(BruteForce)
+// Time Complexity T - O(nk log k)
+// Space Complexity S - O(nk)
+// Where n - no. of string inputs, k - size of the largest array
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        // if string is empy then return empty array
+        if (strs.length == 0) {
+            return new ArrayList();
+        }
+        
+        // Create hashmap to store
+        // The sorted letters as "key" and orginial strings as "values"
+        Map<String, List> map = new HashMap<String, List>();
+        
+        // loop runs for all the strings in "strs"
+        for (String s : strs) {
+            
+            // converting the string to char array
+            char[] charStr = s.toCharArray();
+            
+            // sorting the charStr
+            Arrays.sort(charStr);
+            
+            // converting the sorted charStr to String
+            String key = String.valueOf(charStr);
+            
+            // if the key is not present in the hashmap
+            // then add the key in new arraylist in the hashmap
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList());
+            }
+            
+            // adding up the orginial string as a value to the sorted string 
+            map.get(key).add(s);
+        }
+        
+        // returning the list of Array contains the values of the map
+        return new ArrayList(map.values());
+    }
+}
