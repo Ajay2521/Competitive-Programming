b684609614e38678d5618ebfc41a12b8aa513ead
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-29 10:30:00 +0530

    Update pow(x,n) by Recursive.cpp

--- Integer/pow(x,n)/pow(x,n) by Recursive.cpp
+++ Integer/pow(x,n)/pow(x,n) by Recursive.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-        int N;
             
         // checking whether the "n" is zero
         if(n == 0){
@@ -11,22 +9,14 @@ public:
         
         
         // if "n" is postivite then do the below operation
-        if(n > 0){
-            
-            // if "n" is negative then convert the negative value into pasitive one
-            N =  n;
-        
+        if(n>=1){
+            return x*myPow(x,n-1);;    
         }
         
-        // checking whether "N" is divisible by "2", if so
-        if(N % 2 == 0){
-            return myPow(x, N / 2) * myPow(x, N / 2);
-        }
-        
-        // if "n" is not divisble by
+        // if "n" is negative then do the below operation
         else{
-            
-            return x * myPow(x, N / 2) * myPow(x, N / 2);   
+           
+            return 1/(x*myPow(x,(abs(n)-1)));
         }
 
     }
