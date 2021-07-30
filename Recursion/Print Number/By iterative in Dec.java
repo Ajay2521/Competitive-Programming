983584da276ffed9f524bde3535799300484283f
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-30 10:44:36 +0530

    Descending

---
+++ Recursion/Print Number/By iterative in Dec.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  
+    public static void main(String args[]) {
+        // "n" store value which is to be printed
+	      int n = 5;
+	
+	      // loop runs from i to n
+	      for (int i = n; i>0; i--){
+		      // printing the numbers
+		      System.out.print(i);
+	      }	
+    }
+}
