5669176076274a76ae8e1982158ec01b215a219b
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-30 10:45:35 +0530

    Descending

---
+++ Recursion/Print Number/By iterative in Dec.cpp
@@ -0,0 +1,22 @@
+
+// including header file
+#include<bits/stdc++.h>
+
+// namespace
+using namespace std;
+
+// main function
+int main(){
+	
+	// n stor value which is to be printed
+	int n = 5;
+	
+	// loop runs from i to n
+	for (int i = 1; i<=n; i++){
+		// printing the numbers
+		cout<<i;
+	}	
+	
+	return 0;
+
+}
