7514ea5b7682583c29d119eeff7105bdb6bf7c2f
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-30 10:43:48 +0530

    Ascending

---
+++ Recursion/Print Number/By iterative in Asc.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  
+    public static void main(String args[]) {
+        // "n" store value which is to be printed
+	      int n = 5;
+	
+	      // loop runs from i to n
+	      for (int i = 1; i<=n; i++){
+		      // printing the numbers
+		      System.out.print(i);
+	      }	
+    }
+}
