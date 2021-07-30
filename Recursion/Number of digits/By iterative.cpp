4883dcda70ee5dec432792de023d18a94154d237
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-30 11:05:01 +0530

    GeeksForGeeks

---
+++ Recursion/Number of digits/By iterative.cpp
@@ -0,0 +1,25 @@
+
+// including header file
+#include<bits/stdc++.h>
+
+// namespace
+using namespace std;
+
+// main function
+int main(){
+	
+	// n store digits
+	long long n;
+    int count = 0;
+    cin>>n;
+	
+	// loop runs till "n" becomes 0
+	while (n != 0) {
+        n = n / 10;    
+        count++;
+    }
+    
+    // print the value of count
+	cout<<count;
+	
+}
