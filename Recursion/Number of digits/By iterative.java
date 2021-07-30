2330fdf0dcad1348c14d0f4b28158d6080420a9c
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-07-30 11:07:07 +0530

    GeeksForGeeks

---
+++ Recursion/Number of digits/By iterative.java
@@ -0,0 +1,19 @@
+public class MyClass {
+
+    public static void main(String args[]) {
+        
+      // n store digits
+	    long long n;
+      int count = 0;
+      cin>>n;
+	       
+	    // loop runs till "n" becomes 0
+	    while (n != 0) {
+            n = n / 10;    
+            count++;
+      }
+        
+    // print the value of count
+		System.out.print(i);
+  }
+}
