86c3d529b78d22a13e5b1555c0ddb2b3a12ebd93
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-08-04 08:01:34 +0530

    Leetcode

---
+++ Leetcode And GFG/Array/Easy/Remove Duplicates Sorted Array/By Two Pointers.cpp
@@ -0,0 +1,36 @@
+// Optimised -Two Pointer
+// Time Complexity T - O(n)
+// Space Complexity S - O(1)
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        // ensure the "nums" array is non empty
+        // if empty return 0
+        if(nums.size() == 0){
+            return 0;
+        }
+        
+        // "unique" is incremented for all unique values in an array 
+        int unique = 0;
+        
+        // the loop runs from "1" to "n-1" times 
+        for(int i = 1; i < nums.size(); i++){
+            
+            // Note: array "nums" is in reverse order. 
+            // It is advice to check current element is equal to previous element or not
+            // if the value of nums[i] is not same as the value of previous unique element
+            // then increment the "unique" and 
+            // remove the duplicate by assigning the value of "i" to "unique"
+            if(nums[i] != nums[unique]){
+                unique++;
+                nums[unique] = nums[i];
+            }
+            
+        }
+        
+        // return K , Where K is the size of array with unique values
+        return unique+1;
+    }
+};
