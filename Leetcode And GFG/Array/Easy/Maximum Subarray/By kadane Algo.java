a6d0cfe5a80748494dd4e632f3e4ef701c98054f
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-08-04 08:59:22 +0530

    Leetcode

---
+++ Leetcode And GFG/Array/Easy/Maximum Subarray/By kadane Algo.java
@@ -0,0 +1,49 @@
+// Optimised - Kadane's algorithm(DP)
+// Time Complexity T - O(n), Since loop runs for "n" times
+// Space Complexity S - O(1), runs on constant sapce time
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        // ensure the array "nums" is non empty
+        // if empty return "0"
+        if(nums.length == 0 )
+        {
+            return 0;
+        }
+        
+        // if array "nums" size is "1"
+        // then return the "0"th value of the array
+        if(nums.length == 1 )
+        {
+            return nums[0];
+        }
+        
+        // maxmiumValue will return the maxmium value obtained by the sum of sub array
+        int maximumValue = Integer.MIN_VALUE;
+        
+        // "sum" is used to store the sum of values
+        int sum = 0;
+        
+        // loop runs from "0" to "len(nums)-1"
+        for(int i = 0; i < nums.length; i++)
+        {
+            
+            // adding up the value
+            sum += nums[i]; // sum = sum + nums[i]
+            
+            // if the "sum" is greater than "maxmiumValue"
+            // then assign the value of "sum" to "maxmiumValue"
+            if(maximumValue < sum){
+                maximumValue = sum;
+            }
+            
+            // if "sum" becomes less than "0",
+            // then assign "sum" is "0"
+            if(sum < 0){
+                sum = 0;    
+            }    
+        }
+        return maximumValue;
+    }
+}
