892c4d09bb64f5a5b01d07bc644085b9d6322855
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-08-04 07:44:01 +0530

    Leetcode

---
+++ Leetcode And GFG/Array/Easy/Remove Duplicates Sorted Array/By iteration.java
@@ -0,0 +1,33 @@
+// Brute Force - Iteration
+// Time Complexity T - O(n)
+// Space Complexity S - O(1)
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        // "duplicateCount" is used to count the duplicates in the array "nums"
+        int duplicateCount=0;
+        
+        // the loop runs from "1" to "n-1" times 
+        for(int i = 1; i < nums.length; i++){
+            
+            // Note: array "nums" is in reverse order. 
+            // It is advice to check current element is equal to previous element or not
+            // if the value of nums[i] is same as the value of previous element
+            // then increment the count
+            if(nums[i] == nums[i-1]){
+                duplicateCount++;
+            }
+            
+            // if the current element is not same as the previous element
+            // then remove the previous element (duplicate element)
+            else{
+                nums[i-duplicateCount]=nums[i];
+            }
+        }
+        
+        // return K , Where K is the size of array with unique values
+        return nums.length-duplicateCount;
+    }
+}
