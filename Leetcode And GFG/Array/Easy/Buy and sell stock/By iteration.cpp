d0930f1fbaf4790e2098ed39835624daebb49d33
Ajay <60919132+Ajay2521@users.noreply.github.com>
2021-08-05 10:56:57 +0530

    Leetcode

---
+++ Leetcode And GFG/Array/Easy/Buy and sell stock/By iteration.cpp
@@ -0,0 +1,32 @@
+// BruteForce - Iteration
+// Time Complexity T - O(n^2)
+// Space Complexity S - O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        // initialise the maximumProfit to "0"
+        int maximumProfit = 0;
+        
+        // loop throug the price array from "0" to "n-1"
+        for(int i = 0; i < prices.size() -1 ; i++){
+            
+            // loop throug the price array from "1" to "n"
+            for(int j = i+1; j < prices.size(); j++){
+                
+                // finding the profit for each "i" and "i+1" element
+                int profit = prices[j] - prices[i];
+                
+                // if profit is more than a maximumProfit
+                // then update the maximumProfit with the profit value
+                if( profit > maximumProfit){
+                    
+                    maximumProfit = profit;
+                }
+            }
+        }
+        // return the maximumProfit value
+        return maximumProfit;
+    }
+};
